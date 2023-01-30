a9a22b38056c31ccd23059dc2bb957584f12c404
Gonzalo7476 <92580135+Gonzalo7476@users.noreply.github.com>
2023-01-30 13:17:38 +0100

    Update ProyectoJava2023.java

--- src/main/java/com/mycompany/proyectojava2023/ProyectoJava2023.java
+++ src/main/java/com/mycompany/proyectojava2023/ProyectoJava2023.java
@@ -6,6 +6,6 @@ package com.mycompany.proyectojava2023;
 public class ProyectoJava2023 {
 
     public static void main(String[] args) {
-        System.out.println("Version 1.0: Version base");
+        System.out.println("Version 1.0: Version base. Version 2.0: Nuevos cambios y correccion de errores");
     }
 }
